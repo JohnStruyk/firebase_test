a7bc0a06cb9938c37e1dde34420deb27e6b4d0f3
struy013 <struy013@umn.edu>
2025-11-11 09:11:02 -0600

    vue

---
+++ src/components/CounterButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import { ref } from 'vue'
+    const count = ref(0)
+</script>
+
+<template>
+  <button type="button" @click="count++">
+    Click this button to change a crazy setting {{ count }}
+  </button>
+</template>
+
+<style scoped>
+    button {
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
+    border: 1px solid #ccc;
+    cursor: pointer;
+    }
+</style>
