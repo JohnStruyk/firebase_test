f7f5b21decf4509aeb6a6f98271dd7dde9ffbe86
struy013 <struy013@umn.edu>
2025-11-18 08:22:29 -0600

    fix syntax

--- src/firebase_conf.js
+++ src/firebase_conf.js
@@ -19,4 +19,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const fire_app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(fire_app);
-export const db = getFirestore(firebaseApp)
+export const db = getFirestore(fire_App)
